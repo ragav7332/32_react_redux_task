f0bf9a7fb31bb703488436caef476f1568743665
ragav7332 <ragav7332@gmail.com>
2023-12-03 09:02:15 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
+import { Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='container-fluid'>
+        <NavBar />
+        <Routes>
+          <Route path='/' Component={Home}></Route>
+          <Route path='/cart' Component={Cart}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
